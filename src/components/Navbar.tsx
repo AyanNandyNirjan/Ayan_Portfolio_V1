82b9f022e7595c805f6dc9b0a721343853bc4625
AyanNandyNirjan <ayannandy1408@gmail.com>
2026-05-31 00:32:27 +0600

    updates

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -64,7 +64,7 @@ const Navbar = () => {
             Open
           </div>
           <a
-            href="https://drive.google.com/file/d/1BCng1LYCQ0n6dQakpLAGBWGQv_bu7vLa/view?usp=sharing"
+            href="https://drive.google.com/file/d/1bNQ-I9wON2RLI_Ey8zLn2KFPn14f5LJ6/view?usp=sharing"
             target="_blank"
             rel="noopener noreferrer"
             className="flex items-center gap-2 text-sm font-medium text-foreground bg-card/80 hover:bg-card border border-border hover:border-primary/50 px-5 py-2.5 rounded-full transition-all"
@@ -104,7 +104,7 @@ const Navbar = () => {
             </a>
           ))}
           <a
-            href="https://drive.google.com/file/d/1MtklfkjKBFdCGxbSbPkxYhDfV3tquWsN/view?usp=sharing"
+            href="https://drive.google.com/file/d/1bNQ-I9wON2RLI_Ey8zLn2KFPn14f5LJ6/view?usp=sharing"
             target="_blank"
             rel="noopener noreferrer"
             onClick={() => setMobileOpen(false)}
